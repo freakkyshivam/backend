da7f76febbc98a5b14fa495e9c63352ab6b5e63e
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 20:38:26 +0530

    feat: Add express-session for session management

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
+import 'dotenv/config'
 import express from 'express';
+import session from 'express-session'
 
 import authRouter from './modules/auth/routes/auth.routes.js'
 
 const app = express();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "lax",
+    },
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
