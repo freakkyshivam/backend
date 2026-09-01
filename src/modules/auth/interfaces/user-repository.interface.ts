d28bfddb586731276c35fdc2be340862e3dcc1ca
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 09:59:16 +0530

    feat: Update UserRepository interface to replace saveNewUserInDb with createUser method

--- src/modules/auth/interfaces/user-repository.interface.ts
+++ src/modules/auth/interfaces/user-repository.interface.ts
@@ -2,10 +2,11 @@ import type { User, UserRole } from "../types/auth.types.js";
 
 export interface UserRepository {
   findByEmail(email: string): Promise<User | null>;
-  saveNewUserInDb(
+
+  createUser(
     name: string,
     email: string,
     passwordHash: string,
-    role: UserRole,
-  ): Promise<User | null>;
+    role: UserRole
+  ): Promise<User>;
 }
