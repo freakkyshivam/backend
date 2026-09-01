958329d605ace02c5a71a2f11b24296aa04203af
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 12:34:46 +0530

    feat: Add authentication routes for login and registration

---
+++ src/modules/auth/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { authService } from '../auth.container.js';
+import { AuthController } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+const authController = new AuthController(authService) 
+
+router.post('/login', authController.login);
+router.post('/register', authController.register)
+
+export default router;
