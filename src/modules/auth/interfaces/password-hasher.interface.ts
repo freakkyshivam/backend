0b42405e440e376ef15158915337886b9996ae74
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 10:09:26 +0530

    refactor: Format verify method in PasswordHasher interface for improved readability

--- src/modules/auth/interfaces/password-hasher.interface.ts
+++ src/modules/auth/interfaces/password-hasher.interface.ts
@@ -1,4 +1,8 @@
 export interface PasswordHasher {
   hash(password: string): Promise<string>;
-  verify(password: string, passwordHash: string): Promise<boolean>;
+
+  verify(
+    password: string,
+    passwordHash: string
+  ): Promise<boolean>;
 }
