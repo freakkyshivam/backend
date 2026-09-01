fff9cb007d35e9daf90046b341e4ffe26c8bd569
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 12:53:00 +0530

    feat: Set up Express app with JSON parsing and authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 
+import authRouter from './modules/auth/routes/auth.routes.js'
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/auth', authRouter)
 
 app.get("/", (req, res)=>{
     res.status(200).json("Server is running ")
