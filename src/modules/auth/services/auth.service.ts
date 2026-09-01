a75663ab432836554ebfdc05d346f4502be53139
Shivam Chaudhary <skc722768@gmail.com>
2026-09-01 10:17:59 +0530

    feat: Implement AuthService for user registration and login functionality

---
+++ src/modules/auth/services/auth.service.ts
@@ -0,0 +1,73 @@
+import type { PasswordHasher } from "../interfaces/password-hasher.interface.js";
+import type { PasswordRepository } from "../interfaces/password-repository.interface.js";
+import type { TokenService } from "../interfaces/token-service.interface.js";
+import type { UserRepository } from "../interfaces/user-repository.interface.js";
+import type { loginResult } from "../types/auth.types.js";
+
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordRepository: PasswordRepository,
+    private readonly passwordHasher: PasswordHasher,
+    private readonly tokenService: TokenService,
+  ) {}
+
+  // register new user service (email + password)
+  async register(name: string, email: string, password: string) {
+    const existingUser = await this.userRepository.findByEmail(email);
+
+    if (existingUser) {
+      throw new Error("User with this email already exists");
+    }
+
+    const user = await this.userRepository.createUser(
+      name,
+      email,
+      "normal_user",
+    );
+
+    const passwordHash = await this.passwordHasher.hash(password);
+
+    await this.passwordRepository.create(user.id, passwordHash);
+
+    return user;
+  }
+
+  // login via password service
+  async login(email: string, password: string): Promise<loginResult> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    const passwordH = await this.passwordRepository.findByUserId(user.id);
+
+    if (!passwordH) {
+      throw new Error("Invalid credentials");
+    }
+
+    const valid = await this.passwordHasher.verify(
+      password,
+      passwordH?.passwordHash,
+    );
+
+    if (!valid) {
+      throw new Error("Invalid credentials");
+    }
+
+    const accessToken = this.tokenService.generateAccessToken({
+      userId: user.id,
+      role: user.role,
+    });
+
+    return {
+      accessToken,
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  }
+}
